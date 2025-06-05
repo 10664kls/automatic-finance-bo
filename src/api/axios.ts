0c914794db15caf2152a0656ceab1bdd886e444b
10664kls <phuangpeht.phouminith@krungsri.com>
2025-06-05 10:35:53 +0700

    To init API

---
+++ src/api/axios.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { getTokenFromLocalStorage, setTokenToLocalStorage } from "../utils/storage";
+ 
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 35000, // 35 seconds
+});
+
+API.interceptors.request.use(
+  (config) => {
+    const token = getTokenFromLocalStorage("accessToken");
+    if (token){
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
+
+API.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+          const token = getTokenFromLocalStorage("refreshToken");
+          if (!token) {
+            return Promise.reject(error);
+          }
+
+          const response = await API.post("/v1/auth/token", {
+            token: token
+          });
+
+          if (response.status !== 200) {
+            return Promise.reject(error);
+          }
+
+          const accessToken = response.data.accessToken;
+          const refreshToken = response.data.refreshToken;
+          API.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+            setTokenToLocalStorage(accessToken, refreshToken);
+            return API(originalRequest);
+      } catch (error) {
+          console.error("Error refreshing token:", error);
+          return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default API;
---
+++ src/api/model.ts
@@ -0,0 +1,150 @@
+
+export type ListCalculations = {
+  calculations: Calculation[]
+  nextPageToken: string  
+}
+
+export type calculationStatus = "COMPLETED" | "PENDING" 
+
+export type  Calculation ={
+  id: number
+  statementFileId: number
+  number: string
+  product: string
+  account: Account
+  exchangeRate: number
+  monthlyAverageIncome: number
+  monthlyNetIncome: number
+  totalOtherIncome: number
+  totalBasicSalary: number
+  totalIncome: number
+  periodInMonth: number
+  startedAt: string
+  endedAt: string
+  createdBy: string
+  createdAt: string
+  status:calculationStatus
+  salaryBreakdown: ListMonthlySalaries
+  allowanceBreakdown: ListAllowances
+  commissionBreakdown: ListCommissions
+  source: Source
+}
+
+export type Account ={
+  number: string
+  displayName: string
+  currency: string
+}
+
+export type Metadata = {
+  originalName: string
+  name: string
+  createdBy: string
+  createdAt: string
+  publicUrl: string
+};
+
+export type Source ={
+  basicSalary: breakdown
+  allowance: breakdown
+  commission: breakdown
+  other: breakdown
+}
+
+type breakdown = {
+  monthlyAverage: number
+  total: number
+}
+
+export type ListMonthlySalaries = {
+  monthlySalaries: MonthlySalary[]
+  basicSalary: number
+  total: number
+}
+
+export type MonthlySalary = {
+  month: string
+  timesReceived: number
+  transactions: IncomeTransaction[]
+  total: number
+}
+
+export type ListAllowances = {
+  allowances: Allowance[]
+  total: number
+}
+
+export type Allowance = {
+  title: string
+  months: number
+  monthlyAverage: number
+  transactions: IncomeTransaction[]
+  total: number
+}
+
+export type Commission = {
+	month:                 string           
+	transactions: IncomeTransaction[] 
+	total:                 number
+}
+
+export type ListCommissions = {
+	commissions:    Commission[]
+	monthlyAverage : number
+	total         : number
+}
+
+export type Currency = {
+  id : string 
+  code: string
+  exchangeRate: number
+  createdAt: string
+  updatedAt: string
+}
+
+export type ListCurrencies = {
+  nextPageToken: string 
+  currencies: Currency[]
+}
+
+export type Wording = {
+  id: number
+  word: string
+  category: sourceCategory
+  createdBy: string
+  createdAt: string
+}
+
+export type ListWordings = {
+  nextPageToken: string 
+  wordlists: Wording[]
+}
+
+export type sourceCategory = "SALARY" | "ALLOWANCE" | "COMMISSION" | ""
+export type userStatus = "ENABLED" | "DISABLED" | "CLOSED"
+
+export type User = {
+  isAdmin: boolean
+  id: string
+  email: string
+  displayName: string
+  status: userStatus
+  createdAt: string
+  updatedAt: string
+}
+
+export type ListUsers = {
+  nextPageToken: string 
+  users: User[]
+}
+
+export type IncomeTransaction = {
+  date: string;
+  billNumber: string;
+  noted: string;
+  amount: number
+}
+
+export type ListIncomeTransactions = {
+  transactions: IncomeTransaction[]
+}
